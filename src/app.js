571b47da0bda3646ed61d1c15bcc075bae86772c
mazarafa <arruda.rafa.ra@gmail.com>
2020-07-14 12:47:57 -0400

    crud de api rest

--- src/app.js
+++ src/app.js
@@ -1,17 +1,41 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
 let route = router.get('/',(req,res,next) =>{
     res.status(200).send({
         title:"Api coffee",
-        version:"0.0.1"
+        version:"0.0.2"
 
-    })
-})
+    });
+});
+
+let create = router.post('/',(req,res,next) =>{
+    res.status(201).send(req.body);
+});
+
+let put = router.put('/:id',(req,res,next) =>{
+    const id = req.params.id;
+    res.status(201).send({
+        id:id,
+        item: req.body
+    });
+});
+
+let del = router.delete('/',(req,res,next) =>{
+    res.status(201).send(req.body);
+});
+
 app.use('/',route);
+app.use('/products',create);
+app.use('/products',put);
+app.use('/products',del);
 
 module.exports = app;
